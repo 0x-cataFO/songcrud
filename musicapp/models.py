243846fc899ef0e76838578fcfc32c789389e474
Os Bert <ossym003@gmail.com>
2022-10-28 21:13:50 +0100

    Added models and attributes to music app

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+from email.policy import default
+from xmlrpc.client import DateTime
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+    
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default= datetime.today)
+    likes = models.IntegerField()
+    
+class Lyric(models.Model):
+    content = models.CharField(max_length=800)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
